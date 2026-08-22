3db0f0c5c4bf99fe7c847ea0dc789fb7fa81902f
Gethmin <mathishapriyadarshana94@gmail.com>
2026-08-22 21:29:14 +0530

    feat: add task CRUD API and JWT auth

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.get('/api/health', (req, res) => {
   res.status(200).json({ message: 'CollabBoard API is running!' });
 });
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
 
 app.listen(PORT, () => {
