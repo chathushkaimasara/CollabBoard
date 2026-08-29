88456f14181574067f9026385550323bc05dd914
waranakasupasan <waranaka.s.ogodapola@gmail.com>
2026-08-29 15:58:44 +0530

    feat(auth): create JWT verification middleware for protected routes

--- server/routes/boardRoutes.js
+++ server/routes/boardRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
 
-router.get('/test', (req, res) => res.json({ message: 'Board route working' }));
+router.get('/test', protect, (req, res) => {
+  res.json({ 
+    message: 'Protected Board route working', 
+    userMakingRequest: req.user.userId 
+  });
+});
 
 module.exports = router;
